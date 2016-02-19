88299e8b6c7e67d2544510012205d2b35769f2da
BENGEOS <george.beng@gmail.com>
2016-02-19 16:06:10 +0300

    change structure of report_page class and designs

--- app/src/main/java/deeplife/gcme/com/deeplife/Fragments/Report_Page.java
+++ app/src/main/java/deeplife/gcme/com/deeplife/Fragments/Report_Page.java
@@ -10,73 +10,46 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import deeplife.gcme.com.deeplife.Adapters.ReportItems_Adapter;
+import deeplife.gcme.com.deeplife.Models.ReportItem;
 import deeplife.gcme.com.deeplife.R;
 
 /**
  * Created by BENGEOS on 2/17/16.
  */
 public class Report_Page extends Fragment {
+    private static List<ReportItem> Reports;
+    public Report_Page(){
+        Reports = new ArrayList<ReportItem>();
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.report_page, container, false);
-        Button btn1_Inc,btn1_Dec,btn2_Inc,btn2_Dec,btn_Send;
-        TextView chs1,chs2,chs3;
-        final EditText input1,txt1,txt2;
-        txt1 = (EditText) view.findViewById(R.id.txt_1);
-        txt2 = (EditText) view.findViewById(R.id.txt_2);
+        ListView ReportLists = (ListView) view.findViewById(R.id.report_items);
+        Reports.add(new ReportItem("1"));
+        Reports.add(new ReportItem("2"));
+        Reports.add(new ReportItem("3"));
+        Reports.add(new ReportItem("4"));
 
-        btn1_Inc = (Button) view.findViewById(R.id.btn_inc);
-        btn1_Inc.setOnClickListener(new View.OnClickListener() {
+        ReportLists.setAdapter(new ReportItems_Adapter(getActivity(), Reports));
+        Button btn_Report = (Button) view.findViewById(R.id.btn_send_report);
+        btn_Report.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int x = Integer.parseInt(txt1.getText().toString());
-                if(x<100){
-                    x++;
-                    txt1.setText(""+x);
-                }
-
+                Toast.makeText(getActivity(),"First Report Item Value: "+Reports.get(0).getValue(),Toast.LENGTH_SHORT).show();
             }
         });
-        btn1_Dec = (Button) view.findViewById(R.id.btn_dec);
-        btn1_Dec.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int x = Integer.parseInt(txt1.getText().toString());
-                if(x>0){
-                    x--;
-                    txt1.setText(""+x);
-                }
-            }
-        });
-        btn2_Inc = (Button) view.findViewById(R.id.btn2_inc);
-        btn2_Inc.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int x = Integer.parseInt(txt2.getText().toString());
-                if(x<100){
-                    x++;
-                    txt2.setText(""+x);
-                }
-            }
-        });
-        btn2_Dec = (Button) view.findViewById(R.id.btn2_dec);
-        btn2_Dec.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int x = Integer.parseInt(txt2.getText().toString());
-                if(x>0){
-                    x--;
-                    txt2.setText(""+x);
-                }
-            }
-        });
-        btn_Send = (Button) view.findViewById(R.id.btn_send);
-
-
-
         return view;
+    }
+    public static void Update_Report_Value(int pos,int value){
+        Reports.get(pos).setValue(value);
     }
 }
